1d30d4fb3e6138dd229854b38e1bb2dd24c958db
LainaJ <lainakarosic@gmail.com>
2020-06-16 12:38:39 -0400

    main image

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,14 +1,30 @@
 import React from "react"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby" 
+// import Image from "./image.js"
+
 
 const Banner = () => {
+    const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 300) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
 
     return (
         <div className="banner">
             <div className="container">
                 <div className="row">
                     <div className="main-text">Laina</div>
+                        <div className="main-image">
+                            <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+                        </div>
                 </div>
             </div>
         </div>
