7d529becc351ecd9d5df0b05952e6330024470e9
LainaJ <lainakarosic@gmail.com>
2020-06-19 15:38:11 -0400

    3 projects added

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,17 +11,52 @@ const Projects = () => {
             <div className="video-player">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/tDelWyMwB0A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
-        <div className="video-content">
-          <div className="content">
-          <h4>Pomodoro</h4>
-            <p>
-            A time-management and productivity application inspired by the Pomodoro Technique
-            </p>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
+            <div className="video-content">
+                <div className="content">
+                    <h4>Pomodoro</h4>
+                    <p>A time-management and productivity application inspired by the Pomodoro Technique
+                    </p>
+                </div>
+            </div>
+        </div> {/* end each project*/}
+
+       {/* project 2 */}
+       <div className="inner-project">
+            <div className="video-player">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/XFmfwhXI2ps" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className="video-content">
+                <div className="content">
+                    <h4>Neopests</h4>
+                    <p>A game where users can adopts virtual pests, shop and play</p>
+                </div>
+            </div>
+        </div> {/* end each project*/}
+
+
+        <div className="inner-project">
+            <div className="video-player">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/UMyVeURETLA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <div className="video-content">
+                <div className="content">
+                    <h4>NYC Underground</h4>
+                    <p>A city guide to NYC’s hidden underground tunnels, tours and attractions</p>
+                </div>
+            </div>
+        </div> {/* end each project*/}
+
+
+
+
+
+
+
+
+
+
+      </div>{/* end container div */}
+    </div> //end project section 
   )
 }
 
