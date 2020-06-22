732db13feef5410d5fb2c517a481317ee9a8c890
LainaJ <lainakarosic@gmail.com>
2020-06-22 11:19:45 -0400

    format improvements

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -1,42 +1,44 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby" 
+import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
 import Portrait from "../images/portrait.jpg"
-import '../styles/styles.scss'
+import "../styles/styles.scss"
 
 const AboutMe = () => {
+  //add image here later
+  // const data = useStaticQuery(graphql`
+  // query {
 
-    //add image here later
-    // const data = useStaticQuery(graphql`
-    // query {
-
-    // }
-    // `)
-    return (
+  // }
+  // `)
+  return (
     <div className="about-me">
-        <div className="container">
-            <div className="inner-about">
-                <div className="content">
-                    <h3>About Me</h3>
-                    <p>I'm a NYC-based software developer with a passion for building beautiful & functional websites. My background in the medical field involved designing and building customized solutions and assistive technology for clients. I love creating meaningful products with attentiveness to user experience. 
-                    </p>
-                    <div className="btn-row">
-                        <Link to="/projects">View Projects</Link>
-                    </div>
-                </div>
-                {/* <div className="images"> */}
-                    <div className="main-image">
-                    <img src={Portrait} alt="portrait" />
-                            {/* <Img fluid={data.placeholderImage.childImageSharp.fluid} /> */}
-                    </div>
-                {/* </div> */}
+      <div className="container">
+        <div className="inner-about">
+          <div className="content">
+            <h3>About Me</h3>
+            <p>
+              I'm a NYC-based software developer with a passion for building
+              beautiful & functional websites. My background in the medical
+              field involved designing and building customized solutions and
+              assistive technology for clients. I love creating meaningful
+              products with attentiveness to user experience.
+            </p>
+            <div className="btn-row">
+              <Link to="/projects">View Projects</Link>
             </div>
+          </div>
+          {/* <div className="images"> */}
+          <div className="main-image">
+            <img src={Portrait} alt="portrait" />
+            {/* <Img fluid={data.placeholderImage.childImageSharp.fluid} /> */}
+          </div>
+          {/* </div> */}
         </div>
+      </div>
     </div>
-    )  
-
-    }
-
+  )
+}
 
 export default AboutMe
--- src/components/projects.js
+++ src/components/projects.js
@@ -5,49 +5,75 @@ const Projects = () => {
     <div className="project-section">
       <div className="container">
         <div className="project-heading">
-            <h3>Projects</h3>
+          <h3>Projects</h3>
         </div>
         <div className="inner-project">
-            <div className="video-player">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/tDelWyMwB0A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className="video-player">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/tDelWyMwB0A"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="video-content">
+            <div className="content">
+              <h4>Pomodoro</h4>
+              <p>
+                A time-management and productivity application inspired by the
+                Pomodoro Technique
+              </p>
             </div>
-            <div className="video-content">
-                <div className="content">
-                    <h4>Pomodoro</h4>
-                    <p>A time-management and productivity application inspired by the Pomodoro Technique
-                    </p>
-                </div>
-            </div>
-        </div> {/* end each project*/}
-
-       {/* project 2 */}
-       <div className="inner-project">
-            <div className="video-player">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/XFmfwhXI2ps" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            </div>
-            <div className="video-content">
-                <div className="content">
-                    <h4>Neopests</h4>
-                    <p>A game where users can adopts virtual pests, shop and play</p>
-                </div>
-            </div>
-        </div> {/* end each project*/}
-
-
+          </div>
+        </div>{" "}
+        {/* end each project*/}
+        {/* project 2 */}
         <div className="inner-project">
-            <div className="video-player">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/UMyVeURETLA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <div className="video-player">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/XFmfwhXI2ps"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="video-content">
+            <div className="content">
+              <h4>Neopests</h4>
+              <p>A game where users can adopts virtual pests, shop and play</p>
             </div>
-            <div className="video-content">
-                <div className="content">
-                    <h4>NYC Underground</h4>
-                    <p>A city guide to NYC’s hidden underground tunnels, tours and attractions</p>
-                </div>
+          </div>
+        </div>{" "}
+        {/* end each project*/}
+        <div className="inner-project">
+          <div className="video-player">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/UMyVeURETLA"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div className="video-content">
+            <div className="content">
+              <h4>NYC Underground</h4>
+              <p>
+                A city guide to NYC’s hidden underground tunnels, tours and
+                attractions
+              </p>
             </div>
-        </div> {/* end each project*/}
-
-      </div>{/* end container div */}
-    </div> //end project section 
+          </div>
+        </div>{" "}
+        {/* end each project*/}
+      </div>
+      {/* end container div */}
+    </div> //end project section
   )
 }
 
